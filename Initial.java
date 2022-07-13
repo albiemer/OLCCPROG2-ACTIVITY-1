d1b2cfc292840787f502ca9884e608fb943479f1
albiemer porte <36027987+albiemer@users.noreply.github.com>
2022-07-13 16:22:07 +0800

    Add files via upload

---
+++ Initial.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Name: Abiemer Porte\n");
+		System.out.println("     A          L");
+		System.out.println("    A A         L");
+		System.out.println("   AAAAA        L");
+		System.out.println("  A     A       L");
+		System.out.println(" A       A      LLLLLL");
+		
+	}
+}
